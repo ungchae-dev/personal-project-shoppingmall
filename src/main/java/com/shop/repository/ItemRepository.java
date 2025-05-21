e6ef1d85e615e17ebe1f97d88f62e2ff4a8b2afb
ungchaeNa <ucnadev3@gmail.com>
2025-05-21 22:00:21 +0900

    Add ItemRepository with query method by item_name

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -3,6 +3,20 @@ package com.shop.repository;
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+// 제네릭에는 <1번째 엔티티 타입 클래스, 2번째 기본키 타입> 과 같이 넣어줌.
+
+    // application-test.properties 코드 작성 후 진행 ↓
+    // ItemRepository 마우스 우클릭 -> Go To -> Test -> Create New Test를 통해 JUnit5 라이브러리의 파일 생성.
+    // => [리포지터리명Test]로 src/test 폴더 내부에 파일 생성된 것 확인~
+
+    // 쿼리 메서드 사용 시 find 사용
+    // 형식: find + 엔티티명 + By + 변수명
+    // itemNm(상품명)으로 데이터를 조회하기 위해 findItemByItemNm 사용
+    // 이 때, 엔티티명은 생략 가능해서 findByItemNm 과 같이 메서드명을 지음
+    // 매개변수: 검색 시 사용할 상품명 변수
+    List<Item> findByItemNm(String itemNm);
 
 }
