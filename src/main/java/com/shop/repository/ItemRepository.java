3cb72fdedd4589a7a47f497aa5314ac60b718144
ungchaeNa <ucnadev3@gmail.com>
2025-05-22 22:37:44 +0900

    Implement custom search with @Query in ItemRepository

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.shop.repository;
 
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -27,5 +29,11 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     // 상품의 가격이 높은 순에서 낮은 순(내림차순)으로 조회하는 쿼리 메서드
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    // Item: 엔티티 클래스, Item 에서 데이터를 조회하는 쿼리
+    @Query("select * from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+    // @Param 역할: 파라미터로 넘어온 값을 JPQL에 들어갈 변수로 지정해줄 수 있음.
+    // itemDetail 변수를 "like % %" 에서 % 사이에 :[변수명]으로 값을 할당함.
 
 }
